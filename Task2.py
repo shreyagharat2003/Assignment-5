bdac93220fd0a84757e911f5738bd3ba278acbf2
shreyagharat2003 <shreyagharat2003@gmail.com>
2025-08-29 11:56:52 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,7 @@
+#Demonstrate List Slicing
+numbers =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+first_five = numbers[:5]
+reversed_five = first_five[::-1]
+print("1 to 10 numbers: ", numbers)
+print("First five elements:", first_five)
+print("Reversed list:", reversed_five)
